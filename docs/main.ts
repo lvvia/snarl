cc99c62c55af0fae216a7bd9aebdde2363bf93c5
kyu.re <k@kyu.re>
2026-08-15 12:28:15 -0300

    docs: initial commit

---
+++ docs/main.ts
@@ -0,0 +1,14 @@
+/**
+ * snarl, a minimal web framework for deno
+ * Copyright (c) 2025-2026 kyu.re
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { createApp } from "@404/imouto";
+import { aether } from "@404/aether/server";
+
+const app = await createApp({ routesDir: "./routes" });
+
+app.use(aether({ entrypoints: ["./routes"] }));
+
+app.serve({ port: 8000 });
